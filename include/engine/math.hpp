77fe2a3ea49857ad2cf4dfb6f81c59cb7698e28c
ERMTMG <eduardorhsiete@gmail.com>
2026-04-19 00:17:23 +0200

    added a few more math and vector methods

--- include/engine/math.hpp
+++ include/engine/math.hpp
@@ -1,5 +1,7 @@
 #pragma once 
 
+#include <algorithm>
+#include <cmath>
 #include <cstdlib>
 
 namespace prim {
@@ -13,6 +15,23 @@ inline bool approx_eq(FloatT x_1, FloatT x_2) noexcept {
 template<typename FloatT>
 inline bool approx_eq(FloatT x_1, FloatT x_2, FloatT tolerance) noexcept {
     return (std::abs(x_1 - x_2) < tolerance);
+}
+
+// Clamps `x` between `min` and `max` in-place, possibly modifying the value. Returns `true` iff `x` was modified.
+template<typename FloatT> 
+inline bool clamp_inplace(FloatT& x, FloatT min, FloatT max) noexcept {
+    FloatT clamped_x = std::clamp(x, min, max);
+    if(x == clamped_x) {
+        return false;
+    } else {
+        x = clamped_x;
+        return true;
+    }
+}
+
+template<typename FloatT>
+inline void lerp_inplace(FloatT& from, FloatT to, FloatT t) noexcept {
+    from += (to - from) * t;
 }
 
 }
--- include/engine/vec2.hpp
+++ include/engine/vec2.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <cmath>
 #include <functional>
 #include <limits>
@@ -18,6 +19,13 @@ struct Vec2 {
     // Basic constructor
     inline Vec2(float x, float y) noexcept : x(x), y(y) {};
     
+    // Defaulting copy/move constructors/assignments
+
+    inline Vec2(const Vec2&) noexcept = default;
+    inline Vec2(Vec2&&) noexcept = default;
+    inline Vec2& operator=(const Vec2&) noexcept = default;
+    inline Vec2& operator=(Vec2&&) noexcept = default;
+
     // Converts from Raylib's Vector2
     inline static Vec2 from(Vector2 v) noexcept { return { v.x, v.y }; }
 
@@ -59,10 +67,37 @@ struct Vec2 {
         return { v.x * scalar, v.y * scalar };
     }
 
+    // Special case, because saying `2 * v` is sometimes nicer than `v * 2`.
     friend Vec2 operator*(float scalar, const Vec2& v) noexcept;
 
     inline Vec2 operator/(this const Vec2& v, float scalar) noexcept {
         return { v.x / scalar, v.y * scalar };
+    }
+
+    // Operator assignments (why aren't these overloaded automatically smh)
+
+    inline Vec2& operator+=(this Vec2& v_dest, const Vec2& src) noexcept {
+        v_dest.x += src.x;
+        v_dest.y += src.y;
+        return v_dest;
+    }
+
+    inline Vec2& operator-=(this Vec2& v_dest, const Vec2& src) noexcept {
+        v_dest.x -= src.x;
+        v_dest.y -= src.y;
+        return v_dest;
+    }
+
+    inline Vec2& operator*=(this Vec2& v_dest, float scalar) noexcept {
+        v_dest.x *= scalar;
+        v_dest.y *= scalar;
+        return v_dest;
+    }
+
+    inline Vec2& operator/=(this Vec2& v_dest, float scalar) noexcept {
+        v_dest.x /= scalar;
+        v_dest.y /= scalar;
+        return v_dest;
     }
 
     // Non-operator math functions //
@@ -138,10 +173,95 @@ struct Vec2 {
         return prim::approx_eq(v_1.x, v_2.x) && prim::approx_eq(v_1.y, v_2.y);
     }
 
-    // Converts to Raylib `Vector2` struct.
+    inline bool is_unit() const noexcept {
+        return prim::approx_eq(this->x * this->x + this->y * this->y, 1.f);
+    }
+
+    // Normalizes the vector in-place. Calling this on a vector with length zero is not accounted for.
+    inline void normalize() noexcept {
+        float len = std::sqrt(this->x * this->x + this->y * this->y);
+        this->x /= len;
+        this->y /= len;
+    }
+
+    // version of `.normalized()` that checks if the vector has norm zero, and does nothing if so. 
+    // Returns `true` if the vector was modified (i.e. if it doesn't have length zero)
+    inline bool normalize_checked() noexcept {
+        float len_squared = this->x * this->x + this->y * this->y;
+        if(len_squared != 0.0f && len_squared != -0.0f) {
+            float len = std::sqrt(len_squared);
+            this->x /= len;
+            this->y /= len;
+            return true;
+        }
+        return false;
+    }
+
+    // Returns the result of lerping `from` to `to` by factor `t`.
+    inline Vec2 lerp(this const Vec2& from, const Vec2& to, float t) noexcept {
+        return {
+            std::lerp(from.x, to.x, t),
+            std::lerp(from.y, to.y, t)
+        };
+    }
+
+    // Returns the result of lerping `from` to `to` by factors `ts` component-wise.
+    // That is, the `x` components are lerped by `ts.x` and similarly for the `y`s.
+    // Will this be useful ever?
+    inline Vec2 lerp_component_wise(this const Vec2& from, const Vec2& to, const Vec2& ts) noexcept {
+        return {
+            std::lerp(from.x, to.x, ts.x),
+            std::lerp(from.y, to.y, ts.y)
+        };
+    }
+
+    // Lerps `from` towards `to` by factor `t` inplace, modifying `from`.
+    inline void lerp_inplace(this Vec2& from, const Vec2& to, float t) noexcept {
+        prim::lerp_inplace(from.x, to.x, t);
+        prim::lerp_inplace(from.y, to.y, t);
+    }
+
+    // Lerps `from` towards `to` by factors `ts` component-wise and inplace, modifying `from`.
+    inline void lerp_inplace_component_wise(this Vec2& from, const Vec2& to, const Vec2& ts) noexcept {
+        prim::lerp_inplace(from.x, to.x, ts.x);
+        prim::lerp_inplace(from.y, to.y, ts.y);
+    }
+
+    // Component-wise or Hadamard product. Useful for scaling on non-square proportions.
+    inline Vec2 component_wise_product(this const Vec2& v_1, const Vec2& v_2) noexcept {
+        return {
+            v_1.x * v_2.x,
+            v_1.y * v_2.y
+        };
+    }
+
+    // Is to `.component_wise_product()` what `operator*=` is to `operator*`.
+    inline Vec2& component_wise_multiply(this Vec2& v_1, const Vec2& v_2) noexcept {
+        v_1.x *= v_2.x;
+        v_1.y *= v_2.y;
+        return v_1;
+    }
+
+    // Inverse of component-wise product.
+    inline Vec2 component_wise_division(this const Vec2& v_1, const Vec2& v_2) noexcept {
+        return {
+            v_1.x / v_2.x,
+            v_1.y / v_2.y
+        };
+    }
+
+    // Is to `.component_wise_division()` what `operator/=` is to `operator/`.
+    inline Vec2& component_wise_divide(this Vec2& v_1, const Vec2& v_2) noexcept {
+        v_1.x /= v_2.x;
+        v_1.y /= v_2.y;
+        return v_1;
+    }
+
+    // Converts to Raylib's `Vector2` struct.
     inline ::Vector2 to_rl() const noexcept {
         return ::Vector2 { this->x, this->y };
     }
+
 };
 
 inline const Vec2 Vec2::Zero  = { 0.f,  0.f };
